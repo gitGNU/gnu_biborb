71889bda40a0701f0201f08885ad01f7c598bd4e
Guillaume Gardey <glinmac@gmail.com>
2008-06-08 19:03:41 +0000

    Fix for #20119

--- php/utilities.php
+++ php/utilities.php
@@ -3,102 +3,110 @@
  *
  * This file is part of BibORB
  *
- * Copyright (C) 2003-2007  Guillaume Gardey (glinmac@gmail.com)
+ * Copyright (C) 2003-2007  Guillaume Gardey (ggardey@club-internet.fr)
  *
  * BibORB is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- *
+ * 
  * BibORB is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
+ * 
  */
-
+ 
 /**
- *
+ * 
  * File: utilities.php
- * Author: Guillaume Gardey (glinmac@gmail.com)
+ * Author: Guillaume Gardey (ggardey@club-internet.fr)
  * Licence: GPL
- *
+ * 
  * Description:
- *
- *      Some useful functions
- *
+ * 
+ *      Some useful functions 
+ * 
  */
-
+ 
+/**
+    Use to change the base name of a file, keeping its extension
+    returns the new file name
+ */
+function get_new_name($filename,$newbasename) {
+    $elem = explode('.',$filename);
+    // change the basename
+    $newfilename = $newbasename;
+    // copy the extensions (many possible eg: .ps.gz)
+    for($i=1;$i<count($elem);$i++){
+        $newfilename .= ".".$elem[$i];
+    }
+    return $newfilename;
+}
 
 /**
     Close an HTML page.
  */
 function html_close() {
-    return "</body></html>";
+    return "</body></html>";  
 }
 
 /**
- * Create an HTML header
+    Create an HTML header
  */
-function html_header($iTitle = NULL, $iStyle = NULL, $iBodyClass=NULL, $iInBody=NULL)
-{
-    $aHtml  = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
-    $aHtml .= "\n<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">";
-    $aHtml .= "\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" >";
-    $aHtml .= "\n<head>";
-
+function html_header($title = NULL, $style = NULL, $bodyclass=NULL, $inbody=NULL) {
+    $html  = '<?xml version="1.0" encoding="ISO-8859-1"?>';
+    $html .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">';  
+    $html .= '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >';
+    $html .= "<head>";
     // Charset, content type
-    $aHtml .= "\n\t<meta http-equiv='content-type' content='text/html; charset=utf-8' />";
-
+    $html .= "<meta http-equiv='content-type' content='text/html; charset=ISO-8859-1' />";
     // how to handle robots
-    $aHtml .= "\n\t<meta name='robots' content='noindex,nofollow'/>";
-
+    $html .= "<meta name='robots' content='noindex,nofollow'/>";
     // define the CSS stylesheet
-    if ($iStyle)
-    {
-        $aHtml .= "\n\t<link href='$iStyle' rel='stylesheet' type='text/css'/>";
-    }
-
+    if($style){
+        $html .= "<link href='$style' rel='stylesheet' type='text/css'/>";
+    }  
     // define the title
-    if($iTitle)
-    {
-        $aHtml .= "\n\t<title>$iTitle</title>\n";
+    if($title){
+        $html .= "<title>$title</title>";
     }
-
     // define the javascript ressource
-    $aHtml .= "\n\t<script type='text/javascript' src='./biborb.js'></script>";
-
-    $aHtml .= "\n</head>";
-    $aHtml .= sprintf("\n<body%s%s>",$iBodyClass,$iInBody);
-
-    return $aHtml;
-}
-
-/**
- * Replace > < & ' " with their html representation.
- */
-function specialFiveToHtml($iString)
-{
-    $patterns = array('&','<','>','\'','\"');
-    $replace = array('&amp;','&lt;','&gt;','&apos;','&quot');
-    return str_replace($patterns, $replace, $iString);
-}
-
-/**
- * Replace html version of > < & ' " with their caracter version.
- */
-function specialFiveToText($iString)
-{
-    $replace = array('&','<','>','\'','\"');
-    $patterns = array('&amp;','&lt;','&gt;','&apos;','&quot');
-    return str_replace($patterns, $replace, $iString);
-}
-
+    $html .= "<script type='text/javascript' src='./biborb.js'></script>";
     
+    $html .= "</head>";
+    $html .= "<body";
+    if($bodyclass){    
+        $html .= " class='$bodyclass' ";
+    }
+    if($inbody){
+        $html .= " ".$inbody." ";
+    }
+    $html .= ">";
+    
+    return $html;  
+}
+
+/*
+    Load a text file
+ */
+function load_file($filename) {
+  return implode('',file($filename));  
+}
+
+/**
+    Replace special chars into their HTML representation.
+ */
+function myhtmlentities($str){
+    $patterns = array('&','<','>');
+    $replace = array('&amp;','&lt;','&gt;');    
+    return str_replace($patterns,$replace,$str);
+}
+
 function xhtml_select($name,$size,$tab,$selected,$onchange=null,$style=null,$class=null)
 {
     $result = "<select name='$name' id='$name'";
@@ -124,29 +132,15 @@ function xhtml_select($name,$size,$tab,$selected,$onchange=null,$style=null,$cla
     return $result;
 }
 
+
 /**
- * Remove slashes in a variable (both keys/values)
- * Ref: http://php.net/manual/en/security.magicquotes.disabling.php
+    Apply stripslashes to a variable or an array and returns the result.
+    If $value is an array, stripslashes is recursively called for each element
+ of the array.
  */
-function undoMagicQuotes($array, $topLevel=true)
-{
-    $newArray = array();
-    foreach($array as $key => $value)
-    {
-        if (!$topLevel)
-        {
-            $key = stripslashes($key);
-        }
-        if (is_array($value))
-        {
-            $newArray[$key] = undoMagicQuotes($value, false);
-        }
-        else
-        {
-            $newArray[$key] = stripslashes($value);
-        }
-    }
-    return $newArray;
+function stripslashes_deep($value){
+    $value = is_array($value) ? array_map('stripslashes_deep', $value) : stripslashes($value);
+    return $value;
 }
 
 /**
@@ -199,7 +193,7 @@ if (!function_exists('array_chunk')) {
 /**
     Evaluate a string has PHP code.
     PHP code should be defined between "<?php" and "?>".
-
+ 
  Example:
     eval_php("<div class='aC'>Today is: <?php echo date("d/m/Y")?>. </div>")
  will return: "<div class='aC'>Today is: 01/01/2003. </div>"
@@ -228,7 +222,7 @@ function eval_php($string){
 function remove_null_values($anArray){
     foreach($anArray as $key=>$val){
         if($val == null || trim($val) == ""){
-            unset($anArray[$key]);
+            array_splice($anArray,$key,1);
         }
     }
     return $anArray;
@@ -236,75 +230,71 @@ function remove_null_values($anArray){
 
 /**
  */
-function read_status_html_select($iName, $iSelected)
-{
-    $aStatusValues =  $_SESSION['bibdb']->getReadStatusValues();
-    $aStatusValues = array_combine($aStatusValues, $aStatusValues);
-    array_walk($aStatusValues, 'msg');
-    return  HtmlToolKit::selectTag(array('size' => 1, 'name' => $iName),
-                                   $aStatusValues,
-                                   $iSelected ? $iSelected : 'any');
+function read_status_html_select($name,$selected){
+    $html = "<select size='1' name='$name'>";
+    if($selected == 'any'){
+        $html .= "<option value='any' selected='selected'></option>";
+    }
+    else{
+        $html .= "<option value='any'></option>";
+    }
+    if($selected == 'read'){
+        $html .= "<option value='read' selected='selected'>".msg("Read")."</option>";
+    }
+    else{
+        $html .= "<option value='read'>".msg("Read")."</option>";
+    }
+    if($selected == 'readnext'){
+        $html .= "<option value='readnext' selected='selected'>".msg("Read Next")."</option>";
+    }
+    else{
+        $html .= "<option value='readnext'>".msg("Read Next")."</option>";
+    }
+    if($selected == 'notread'){
+        $html .= "<option value='notread' selected='selected'>".msg("Not Read")."</option>";
+    }
+    else{
+        $html .= "<option value='notread'>".msg("Not Read")."</option>";
+    }
+    $html .= "</select>";
+    return $html;
 }
-
-
 /**
  */
-function ownership_html_select($iName, $iSelected)
-{
-    $aOwnershipValues =  $_SESSION['bibdb']->getOwnershipValues();
-    $aOwnershipValues = array_combine($aOwnershipValues, $aOwnershipValues);
-    array_walk($aOwnershipValues, 'msg');
-    return HtmlToolKit::selectTag(array('size' => 1, 'name' => $iName),
-                                  $aOwnershipValues,
-                                  $iSelected ? $iSelected : 'any');
-}
-
-/**
- * Return the major PHP version number
- */
-function php_major_version_number() {
-	$tab = explode('.',phpversion());
-	return (int) $tab[0];
-}
-
-function is_php5() {
-	return php_major_version_number() == 5;
-}
-
-function is_php4() {
-	return php_major_version_number() == 4;
-}
-
-/**
- * Unset the variable and set it to false.
- * 
- * We then can use if($ioData) ... without checking with isset
- */
-function myUnset(&$ioData)
-{
-    unset($ioData);
-    $ioData = null;
-}
-
-function myUnsetArray(&$ioArray, $iKey)
-{
-    unset($ioArray[$iKey]);
-}
-
-if (version_compare(phpversion(), "5.0.0", "<") )
-{
-    function array_combine($keys,$vals)
-    {
-        $keys = array_values( (array) $keys );
-        $vals = array_values( (array) $vals );
-        $n = max( count( $keys ), count( $vals ) );
-        $r = array();
-        for( $i=0; $i<$n; $i++ )
-        {
-            $r[ $keys[ $i ] ] = $vals[ $i ];
-        }
-        return $r;
-    }    
+function ownership_html_select($name,$selected){
+    $html = "<select size='1' name='$name'>";
+    if($selected == 'any'){
+        $html .= "<option value='any' selected='selected'></option>";
+    }
+    else{
+        $html .= "<option value='any'></option>";
+    }
+    if($selected == 'notown'){
+        $html .= "<option value='notown' selected='selected'>".msg("Not Own")."</option>";
+    }
+    else{
+        $html .= "<option value='notown'>".msg("Not Own")."</option>";
+    }
+    if($selected == 'borrowed'){
+        $html .= "<option value='borrowed' selected='selected'>".msg("Borrowed")."</option>";
+    }
+    else{
+        $html .= "<option value='borrowed'>".msg("Borrowed")."</option>";
+    }
+    if($selected == 'buy'){
+        $html .= "<option value='buy' selected='selected'>".msg("Buy")."</option>";
+    }
+    else{
+        $html .= "<option value='buy'>".msg("Buy")."</option>";
+    }
+    if($selected == 'own'){
+        $html .= "<option value='own' selected='selected'>".msg("Own")."</option>";
+    }
+    else{
+        $html .= "<option value='own'>".msg("Own")."</option>";
+    }
+    $html .= "</select>";
+    return $html;
 }
 
 ?>
