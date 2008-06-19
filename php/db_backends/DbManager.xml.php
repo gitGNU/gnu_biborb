a159a3e04ade5198293972566a523d9f5a7141ed
Guillaume Gardey <glinmac@gmail.com>
2008-06-19 20:56:22 +0000

    Change contact point email Move to GPL v3

--- php/db_backends/DbManager.xml.php
+++ php/db_backends/DbManager.xml.php
@@ -1,31 +1,26 @@
 <?php
 /**
- *
  * This file is part of BibORB
+ * 
+ * Copyright (C) 2003-2008 Guillaume Gardey <glinmac+biborb@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
  *
- * Copyright (C) 2007  Guillaume Gardey (glinmac@gmail.com)
- *
- * BibORB is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * BibORB is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
  */
 
 /**
- *
  * File: DbManager.xml.php
- * Author: Guillaume Gardey (glinmac@gmail.com)
- * Licence: GPL
  *
  * Description:
  *
